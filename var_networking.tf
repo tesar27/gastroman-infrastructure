0e58387e9fcf68b1b24038e46d103a7632b539e4
tesar27 <tesar.public@gmail.com>
2026-02-19 15:03:45 +0100

    subnets added

--- var_networking.tf
+++ var_networking.tf
@@ -1,8 +1,33 @@
-variable "vpc_cidr_block" {
+variable "gastroman_vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "gastroman_public_subnet_1_cidr" {
+  description = "CIDR block for the first public subnet"
+  type        = string
+  default     = "10.0.1.0/28"
+}
+
+variable "gastroman_private_subnet_1_cidr" {
+  description = "CIDR block for the first private subnet"
+  type        = string
+  default     = "10.0.2.0/28"
+}
+
+variable "gastroman_public_subnet_2_cidr" {
+  description = "CIDR block for the second public subnet"
+  type        = string
+  default     = "10.0.3.0/28"
+}
+
+variable "gastroman_private_subnet_2_cidr" {
+  description = "CIDR block for the second private subnet"
+  type        = string
+  default     = "10.0.4.0/28"
+}
+
 
 variable "aws_availability_zone_a" {
   type        = string
