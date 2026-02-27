928ff3b6b1a804516bbb78f414961a7b12be0834
tesar27 <tesar.public@gmail.com>
2026-02-27 11:21:02 +0100

    removing https lb listener for now

--- alb.tf
+++ alb.tf
@@ -32,15 +32,3 @@ resource "aws_lb_listener" "http_80" {
     }
   }
 }
-
-resource "aws_lb_listener" "https_443" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.app_cert.arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
