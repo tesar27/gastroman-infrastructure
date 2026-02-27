d353f9d67638f789b92d041daa4ea9148ed1b46d
tesar27 <tesar.public@gmail.com>
2026-02-27 11:14:54 +0100

    feat: shift EC2 webserver to private subnet

--- networking_subnets.tf
+++ networking_subnets.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "gastroman_public_subnet_1" {
-  vpc_id                  = aws_vpc.gastroman_vpc.id
-  cidr_block              = var.gastroman_public_subnet_1_cidr
-  availability_zone       = var.aws_availability_zone_a
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.gastroman_vpc.id
+  cidr_block        = var.gastroman_public_subnet_1_cidr
+  availability_zone = var.aws_availability_zone_a
+  #map_public_ip_on_launch = true
 
   tags = {
     Name        = "gastroman-public-subnet-1"
@@ -11,10 +11,10 @@ resource "aws_subnet" "gastroman_public_subnet_1" {
 }
 
 resource "aws_subnet" "gastroman_public_subnet_2" {
-  vpc_id                  = aws_vpc.gastroman_vpc.id
-  cidr_block              = var.gastroman_public_subnet_2_cidr
-  availability_zone       = var.aws_availability_zone_b
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.gastroman_vpc.id
+  cidr_block        = var.gastroman_public_subnet_2_cidr
+  availability_zone = var.aws_availability_zone_b
+  #map_public_ip_on_launch = true
   tags = {
     Name        = "gastroman-public-subnet-2"
     Environment = local.environment
