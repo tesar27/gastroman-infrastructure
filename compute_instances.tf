c564dcab2b95e9b18770c933d7fc323f8273a8f1
tesar27 <tesar.public@gmail.com>
2026-02-27 11:15:45 +0100

    feat: add Bastion Host for SSH access to private resources

--- compute_instances.tf
+++ compute_instances.tf
@@ -12,3 +12,19 @@ resource "aws_instance" "gastroman_web_server" {
     Environment = terraform.workspace
   }
 }
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.amazon_linux_2023.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.gastroman_public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+  key_name               = var.key_pair_name
+
+  tags = {
+    Name = "bastion"
+  }
+}
+
+resource "aws_eip" "bastion_eip" {
+  instance = aws_instance.bastion.id
+}
